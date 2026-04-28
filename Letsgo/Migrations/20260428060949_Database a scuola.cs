c2b8a8058542ee8da478a5253b96ad93bcbdd090
TARTARI THOMAS <TARTARI.21116>
2026-04-28 09:03:47 +0200

    Migliorie Grafiche

---
+++ Letsgo/Migrations/20260428060949_Database a scuola.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Letsgo.Migrations
+{
+    /// <inheritdoc />
+    public partial class Databaseascuola : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
